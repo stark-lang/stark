a39495ec24623b9a451d3b90129365db4983483b
Alexandre Mutel <alexandre_mutel@live.com>
2019-09-25 20:59:48 +0200

    Update build.cmd for core lib

--- src/runtime/core/build.cmd
+++ src/runtime/core/build.cmd
@@ -1,3 +1,4 @@
 @echo off
-cd %~dp0
+pushd %~dp0
 dotnet ..\..\compiler\starkc\Bin\Debug\netcoreapp2.1\starkc.dll -nostdlib -target:library -unsafe -langversion:8.0 -recurse:*.sk -runtimemetadataversion:v1.0.0 -out:core.sklib
+popd
